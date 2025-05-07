873a76e49955687242a38212e7e925220906149a
2509roblox <2509roblox@gmail.com>
2025-05-07 10:09:53 +0700

    theme

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -2,60 +2,65 @@
     <div class="wave-container">
         <div class="wave-container">
             <img src="/logo/Frame_90-removebg-preview.png" style="width: 100%;" alt="">
-          </div>
-      </div>
+        </div>
+    </div>
     <div id="footer">
 
         <div class="footer-top">
             <div class="container">
                 <div class="row text-center text-md-start align-items-center">
 
-                  <!-- Column 1: Info -->
-                  <div class="col-md-4 mb-4">
-                    <h5>NHÀ HÀNG THÁI</h5>
-                    <p>Cityland Park Hills, Phường 11, Quận Gò Vấp,<br>
-                      Thành phố Hồ Chí Minh, Việt Nam</p>
-                    <p>Hotline: <span class="highlight">{{ $settings->web_phone }}</span></p>
-                    <p>Email: <a style="color: #fff;" href="mailto:{{ $settings->web_email }}">{{ $settings->web_email }}</a></p>
-                    <div class="social-icons mt-3">
-                      <a href="#"><i class="fab fa-facebook-f"></i></a>
-                      <a href="#"><i class="fab fa-instagram"></i></a>
-                      <a href="#"><i class="fab fa-youtube"></i></a>
-                      <a href="#"><i class="fab fa-tiktok"></i></a>
+                    <!-- Column 1: Info -->
+                    <div class="col-md-4 mb-4">
+                        <h5>NHÀ HÀNG THÁI</h5>
+                        <p>Cityland Park Hills, Phường 11, Quận Gò Vấp,<br>
+                            Thành phố Hồ Chí Minh, Việt Nam</p>
+                        <p>Hotline: <span class="highlight">{{ $settings->web_phone }}</span></p>
+                        <p>Email: <a style="color: #fff;"
+                                href="mailto:{{ $settings->web_email }}">{{ $settings->web_email }}</a></p>
+                        <div class="social-icons mt-3">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-youtube"></i></a>
+                            <a href="#"><i class="fab fa-tiktok"></i></a>
+                        </div>
                     </div>
-                  </div>
 
-                  <!-- Column 2: Logo + Text -->
-                  <div class="col-md-4 mb-4 text-center">
-                    <img src="/logo/logo_footer.png" alt="Logo" class="logo mb-3"> <!-- Thay link này bằng logo của bạn -->
-                    <p>©2025 <strong>Khay–Pha Thai</strong>.</p>
-                    <p>Design by <strong>Vinasoftware (VNS)</strong></p>
-                  </div>
-
-                  <!-- Column 3: Facebook card -->
-                  <div class="col-md-4 mb-4">
-                    <h5>FACEBOOK</h5>
-                    <div class="fb-card p-3 mt-3">
-                      <img src="/logo/fanpage.png" alt="Logo" class="brand-logo me-2"> <!-- Avatar -->
+                    <!-- Column 2: Logo + Text -->
+                    <div class="col-md-4 mb-4 text-center">
+                        <img src="/logo/logo_footer.png" alt="Logo" class="logo mb-3">
+                        <!-- Thay link này bằng logo của bạn -->
+                        <p>©2025 <strong>Khay–Pha Thai</strong>.</p>
+                        <p>Design by <strong>Vinasoftware (VNS)</strong></p>
                     </div>
-                  </div>
+
+                    <!-- Column 3: Facebook card -->
+                    <div class="col-md-4 mb-4">
+                        <h5>FACEBOOK</h5>
+                        <div class="fb-card p-3 mt-3">
+                            <img src="/logo/fanpage.png" alt="Logo" class="brand-logo me-2"> <!-- Avatar -->
+                        </div>
+                    </div>
 
                 </div>
 
-              </div>
+            </div>
         </div>
     </div>
 
     {!! $settings->map_iframe !!}
+
+    <a class="btn-phone btn-frame text-decoration-none" href="tel:0918606032">
+        <i><img src="{{ asset('logo/Facebook_Logo_(2019).png') }}" alt="Hotline"></i>
+    </a>
     <a class="btn-zalo btn-frame text-decoration-none" target="_blank" href="https://zalo.me/0918606032">
         <div class="animated infinite zoomIn kenit-alo-circle"></div>
         <div class="animated infinite pulse kenit-alo-circle-fill"></div>
-        <i><img src="{{ asset('assets/images/zl.png') }}" alt="Zalo"></i>
+        <i><img src="{{ asset('logo/image 20.png') }}" alt="Hotline"></i>
+
     </a>
-    <a class="btn-phone btn-frame text-decoration-none" href="tel:0918606032">
-        <div class="animated infinite zoomIn kenit-alo-circle"></div>
-        <div class="animated infinite pulse kenit-alo-circle-fill"></div>
-        <i><img src="{{ asset('assets/images/hl.png') }}" alt="Hotline"></i>
+    <a class="btn-insta btn-frame text-decoration-none" target="_blank" href="https://zalo.me/0918606032">
+        <i><img src="{{ asset('logo/v982-d3-04 1.png') }}" alt="Zalo"></i>
     </a>
     <div id="messages-facebook"></div>
 
@@ -77,7 +82,8 @@
                 <a target="_blank"
                     href="https://www.google.com/maps/dir/?api=1&origin=&destination=89 Nguyễn Thị Búp, phường Tân Chánh Hiệp, quận 12, TP. HCM"
                     title="Map">
-                    <img src="{{ asset('assets/images/fp-chiduong.png') }}" alt="images"><br>
+                    <img src="{{ asset('assets/images/fp-zalo.png') }}" alt="images"><br>
+
                     <span>Chỉ Đường</span>
                 </a>
             </li>
@@ -100,11 +106,38 @@
     </div>
     <style>
         iframe {
-    width: 100%;
-}
-#footer {
- background: linear-gradient(180deg, #015436, #004f32);
+            width: 100%;
+        }
 
+        #footer {
+            background: linear-gradient(180deg, #015436, #004f32);
+
+        }
+
+        .btn-insta {
+            bottom: 145px;
+        }
+
+        .btn-frame i img {
+            vertical-align: middle;
+            width: 100%;
+        }
+
+        .btn-phone {
+            bottom: 210px;
+        }
+
+        .progress-wrap::after {
+
+            color: #e3b505;
+
+        }
+
+        .progress-wrap svg.progress-circle path {
+            stroke: #e3b505;
+        }
+        #footer {
+    background: #015134;
 }
     </style>
 </div>
