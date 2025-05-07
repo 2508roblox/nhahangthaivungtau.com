fdeb7c8b3cfe1444bb56aa0230ea6846363bdc8c
2509roblox <2509roblox@gmail.com>
2025-05-07 21:00:28 +0700

    ádfasf

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,22 +1,54 @@
-<div>
+<div id="header__">
+    <style>
+        .fix_header {
+            position: fixed;
+            top: 0;
+            z-index: 9999;
+            width: 100%;
+            margin-bottom: 100px;
+        }
+        @media (max-width: 768px) {
+            .right-menu {
+                display: none !important;
+            }
+            .left-menu {
+                display: none !important;
+            }
+        }
+    </style>
+    <script>
+        window.addEventListener('scroll', function () {
+            const header = document.getElementById('header__');
+            if (window.scrollY > 0) {
+                header.classList.add('fix_header');
+            } else {
+                header.classList.remove('fix_header');
+            }
+        });
+
+    </script>
+
 
     <div id="menu" style="background: #003D26; padding: 0;">
         <div class="center d-flex align-items-center justify-content-between">
-            <div class="left-menu">
+            <div class="left-menu" style="    gap: 10px;    display: flex;    flex-direction: column; ">
                 <div style="color: #fff;">
-                    <i class="fas fa-map-marker-alt"></i> {{ $settings->web_address }}
+                    {{ $settings->web_address }}
                 </div>
                 <ul class="d-flex align-items-center justify-content-start" style="color: #fff;">
                     <li>
-                        <a style="background: {{ request()->routeIs('home') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;" class="transition {{ request()->routeIs('home') ? 'active' : '' }}"
+                        <a style="background: {{ request()->routeIs('home') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;"
+                            class="transition {{ request()->routeIs('home') ? 'active' : '' }}"
                             href="{{ route('home') }}" title="Trang chủ" style="color: #fff;">Trang chủ</a>
                     </li>
                     <li>
-                        <a style="background: {{ request()->routeIs('about') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;" class="transition {{ request()->routeIs('about') ? 'active' : '' }}"
+                        <a style="background: {{ request()->routeIs('about') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;"
+                            class="transition {{ request()->routeIs('about') ? 'active' : '' }}"
                             href="{{ route('about') }}" title="Giới thiệu" style="color: #fff;">Giới thiệu</a>
                     </li>
-                    <li   class="menu-item-has-children">
-                        <a style="background: {{ request()->routeIs('food.*') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;" class="transition {{ request()->routeIs('food.*') ? 'active' : '' }}"
+                    <li class="menu-item-has-children">
+                        <a style="background: {{ request()->routeIs('food.*') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;"
+                            class="transition {{ request()->routeIs('food.*') ? 'active' : '' }}"
                             href="{{ route('food.list') }}" title="Món ăn" style="color: #fff;">Món ăn</a>
                         <ul class="sub-menu">
                             @foreach($categories as $category)
@@ -44,7 +76,7 @@
             <div class="   d-flex align-items-center" id="logo_">
                 <a href="{{ route('home') }}">
                     <img onerror="this.src='/thumbs/148x60x2/assets/images/noimage.png';"
-                        src="{{ Storage::url($settings->web_logo) }}" alt="{{ $settings->web_name }}"
+                        src="/logo/01JTJVFBT41JJ8DDNT7X3RFB0B.png" alt="{{ $settings->web_name }}"
                         style="width: initial;object-fit: contain;max-height: 150px;" />
                 </a>
             </div>
@@ -76,27 +108,31 @@
                     }
                 }
             </style>
-            <div class="right-menu">
-                <div class="right" style="display: flex; align-items: center; gap: 10px; color: #fff;">
+            <div class="right-menu" style="    gap: 10px;    display: flex;    flex-direction: column; ">
+                <div class="right"
+                    style="display: flex;align-items: center;gap: 10px;color: #fff;width: 100%;justify-content: end;">
                     <div style="display: flex; align-items: center; gap: 5px; white-space: nowrap; ">
-                        <i class="fas fa-phone"></i> Hotline: {{ $settings->web_phone }}
+                        Hotline: {{ $settings->web_phone }}
                     </div>
                     <div style="display: flex; align-items: center; gap: 5px; white-space: nowrap;">
-                        <i class="far fa-envelope"></i> Email: {{ $settings->web_email }}
+                        | Email: {{ $settings->web_email }}
                     </div>
                 </div>
                 <ul class="d-flex align-items-center justify-content-start">
                     <li>
-                        <a style="background: {{ request()->routeIs('video.list') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;" class="transition {{ request()->routeIs('video.list') ? 'active' : '' }}"
+                        <a style="background: {{ request()->routeIs('video.list') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;"
+                            class="transition {{ request()->routeIs('video.list') ? 'active' : '' }}"
                             href="{{ route('video.list') }}" title="Review ẩm thực" style="color: #fff;">Review ẩm
                             thực</a>
                     </li>
                     <li>
-                        <a style="background: {{ request()->routeIs('album.list') || request()->routeIs('album.*') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;" class="transition {{ request()->routeIs('album.list') || request()->routeIs('album.*') ? 'active' : '' }}"
+                        <a style="background: {{ request()->routeIs('album.list') || request()->routeIs('album.*') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;"
+                            class="transition {{ request()->routeIs('album.list') || request()->routeIs('album.*') ? 'active' : '' }}"
                             href="{{ route('album.list') }}" title="Album ảnh" style="color: #fff;">Album ảnh</a>
                     </li>
                     <li>
-                        <a style="background: {{ request()->routeIs('news.list') || request()->routeIs('news.*') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;" class="transition {{ request()->routeIs('news.list') || request()->routeIs('news.*') ? 'active' : '' }}"
+                        <a style="background: {{ request()->routeIs('news.list') || request()->routeIs('news.*') ? 'url("/logo/image_2025-05-07_204305047-removebg-preview.png")' : 'url("/logo/image_2025-05-07_204647669-removebg-preview.png")' }} no-repeat center center; background-size: 100% 100%; color: #fff; width: 100%;"
+                            class="transition {{ request()->routeIs('news.list') || request()->routeIs('news.*') ? 'active' : '' }}"
                             href="{{ route('news.list') }}" title="Cẩm nang" style="color: #fff;">Cẩm nang</a>
                     </li>
 
@@ -117,7 +153,7 @@
             <ul>
                 <li class="heading">Chuyên mục</li>
                 <li>
-                    <a  class="transition active" href="{{ route('home') }}" title="Trang chủ">Trang chủ</a>
+                    <a class="transition active" href="{{ route('home') }}" title="Trang chủ">Trang chủ</a>
                 </li>
                 <li>
                     <a class="transition " href="{{ route('about') }}" title="Giới thiệu">Giới thiệu</a>
